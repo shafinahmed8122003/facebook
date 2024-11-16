9ea303c83bbb27e0a66fa1414c3d673712d72753
Shafin <shafin.ahmed.8122003@gmail.com>
2024-11-16 19:52:11 +0600

    Custom types defined

--- main.c
+++ main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include "types.h"
 
 int main()
 {
-    printf("Hello world!\n");
+
     return 0;
 }
--- types.h
+++ types.h
@@ -3,10 +3,48 @@
 
 typedef struct
 {
+    int id;
     char name[50];
     char username[50];
     char password[50];
     int profile_locked; // 0 - False & 1 - True
+    int posts_id[500];
+    int friends_id[500];
+    int notifications_id[500];
 } User;
+
+typedef struct
+{
+    int id;
+    char content[500];
+    unsigned long long int timestamp;
+    int author_id;
+    int likes;
+    int comments_id[500];
+    int shares;
+} Post;
+
+typedef struct
+{
+    int id;
+    char content[500];
+    unsigned long long int timestamp;
+    int author_id;
+} Comment;
+
+typedef struct
+{
+    int id;
+    char content[500];
+    unsigned long long int timestamp;
+} Notification;
+
+typedef struct
+{
+    User users[500];
+    Post posts[1000000];
+    Comment comments[1000000];
+    Notification notifications[1000000];
+} Database;
 
 #endif
